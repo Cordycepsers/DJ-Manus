ce0e3d86c71446b7b799768b632e040a42138b3f
Maja LeMaja <majalemaja@pm.me>
2025-10-23 00:54:25 +0200

    Add Notion API configuration to config.sh
    Added Notion API configuration parameters for integration.
    
    Signed-off-by: Maja LeMaja <majalemaja@pm.me>

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # --- YouTube API Configuration ---
-# Get these from the Google Cloud Console (https://console.cloud.google.com/ )
-# You'll need to create a project, enable the YouTube Data API v3, and create OAuth 2.0 credentials.
 CLIENT_ID="YOUR_CLIENT_ID.apps.googleusercontent.com"
 CLIENT_SECRET="YOUR_CLIENT_SECRET"
 
+# --- Notion API Configuration ---
+# Get these from your Notion integration settings
+NOTION_API_KEY="secret_YOUR_NOTION_API_KEY"
+# The ID is in the URL of your Notion database page
+NOTION_DATABASE_ID="YOUR_NOTION_DATABASE_ID"
+
 # --- Playlist Configuration ---
-# The name of the playlist that will be created. The date will be added automatically.
 PLAYLIST_NAME="Manus DJ"
 
 # --- Manus API (Simulation) ---
-# This is a placeholder for the prompt we send to Manus.
-# In a real system, this might be a call to an actual API endpoint.
-# For now, we simulate the response directly in the AppleScript.
 MANUS_PROMPT="Create a playlist that bridges the gap between the raw energy of post-punk and the atmospheric detail of trip-hop."
